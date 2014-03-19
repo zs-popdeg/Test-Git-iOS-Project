3e09da1124ebc3ad27ef1d8ddb9734798a965ee7
Diego Hedman <diegohedman@Pops-Mbp.local>
2014-03-19 10:32:09 +0100

    added a test class

---
+++ ZSTest.h
@@ -0,0 +1,13 @@
+//
+//  ZSTest.h
+//  Git Test
+//
+//  Created by Diego Hedman on 2014-03-19.
+//  Copyright (c) 2014 Zorriz Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZSTest : NSObject
+
+@end
---
+++ ZSTest.m
@@ -0,0 +1,13 @@
+//
+//  ZSTest.m
+//  Git Test
+//
+//  Created by Diego Hedman on 2014-03-19.
+//  Copyright (c) 2014 Zorriz Software. All rights reserved.
+//
+
+#import "ZSTest.h"
+
+@implementation ZSTest
+
+@end
